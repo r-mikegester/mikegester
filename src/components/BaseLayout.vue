36ae13d92cf6ba948df93905fd2a940d38ccb833
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-15 00:26:00 +0800

    Add Another Placeholder Files

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+import FooterSection from "./FooterSection.vue"
+import NavBar from "./NavigationBar.vue"
+import SideBar from "./SideBar.vue"
+</script>
+
+<template>
+  <div
+    class="grid grid-rows-[auto_1fr_auto] min-h-screen text-sky-600 font-[Poppins]"
+  >
+    <NavBar />
+    <SideBar/>
+    <FooterSection />
+  </div>
+</template>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="bg-black w-64 h-full">
+    </div>
+</template>
