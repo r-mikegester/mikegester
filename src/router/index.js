e56fc68117a1be7f94a40b419a6d5a05543ddc98
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-16 00:37:26 +0800

    Index.js removes homepage replaced with App

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomePage from "../views/HomePage.vue"
+import HomePage from "../App.vue"
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
