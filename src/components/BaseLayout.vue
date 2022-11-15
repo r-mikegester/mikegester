788ab8f84009496dc22f329228412d1b35a1b5e8
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-16 00:32:11 +0800

    BaseLayout.vue remove navbar implementation

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -1,15 +1,12 @@
 <script setup>
 import FooterSection from "./FooterSection.vue"
-import NavBar from "./NavigationBar.vue"
+
 import SideBar from "./SideBar.vue"
 </script>
 
 <template>
-  <div
-    class="grid grid-rows-[auto_1fr_auto] min-h-screen text-sky-600 font-[Poppins]"
-  >
-    <NavBar />
-    <SideBar/>
+  <div class="grid grid-rows-[auto_1fr_auto] min-h-screen text-sky-600 font-[Poppins]">
+    <SideBar />
     <FooterSection />
   </div>
 </template>
