d309a7ed11d94991ea3bdc6215790082ffb1fef7
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-16 00:30:13 +0800

    Implementation of Darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  darkMode: 'class', // false, 'class', 'media'
+  // ...
   content: [ "./index.html",
   "./src/**/*.{vue,js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js"], 
   theme: {
     extend: {},
   },
-  plugins: [ require('flowbite/plugin')],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
