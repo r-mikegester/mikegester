7405fdd3a8f273ba2d7520aa13d97e241cbbe340
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-15 08:49:29 +0800

    Added Eslint and Eslint vue plugin

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,5 +3,25 @@ import BaseLayout from "../components/BaseLayout.vue"
 </script>
 <template>
     <BaseLayout/>
-    <h1>Hellowrold</h1>
+    <div class="bg-teal-500 w-full h-full border-teal-500 float-right">wssasssaesadws</div>
+
+  
+    <p>
+      <a href="https://vitejs.dev/guide/features.html" target="_blank">
+        Vite Documentation
+      </a>
+      |
+      <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    </p>
+  
 </template>
+
+  
+ 
+  
+  <style scoped>
+  a {
+    color: #42b983;
+  }
+  </style>
+  
