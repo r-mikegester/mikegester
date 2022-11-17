e61b942c044ffcdd5bd722aecdef487e902dada1
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-17 12:50:16 +0800

    Added TailwindCustom Scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,53 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   darkMode: 'class', // false, 'class', 'media'
   // ...
-  content: [ "./index.html",
-  "./src/**/*.{vue,js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js"], 
+  content: ["./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { "50": "#ecfdf5", "100": "#d1fae5", "200": "#a7f3d0", "300": "#6ee7b7", "400": "#34d399", "500": "#10b981", "600": "#059669", "700": "#047857", "800": "#065f46", "900": "#064e3b" }
+      }
+    },
+    fontFamily: {
+      'body': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      'sans': [
+        'Inter',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'system-ui',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    }
+
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'), ('tailwind-scrollbar'),
   ],
 }
