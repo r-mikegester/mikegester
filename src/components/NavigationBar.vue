0695551460ed76a14c560d32eadc00aefafa1aa6
Mike Gester <mikegester.sabuga023@gmail.com>
2022-11-19 15:25:27 +0800

    Migration of Header profile to NavigationBar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,36 +1,95 @@
 <script setup>
-    
+
 </script>
 
 <template>
 
-<nav class="p-3 bg-gray-50 rounded border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-  <div class="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="#" class="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-10" alt="Flowbite Logo">
-        <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-    </a>
-    <button data-collapse-toggle="navbar-solid-bg" type="button" class="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500" aria-controls="navbar-solid-bg" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-      <ul class="flex flex-col mt-4 bg-gray-50 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+  <div class="w-7/12 bg-white border rounded-lg shadow-md mr-96 ml-72 my-7 dark:bg-gray-800 dark:border-gray-700">
+    <img class="w-full h-64 bg-cover rounded-t-md brightness-75" src="../assets/imgs/sierra.jpg" />
+    <div class="absolute mt-5 text-4xl font-black text-white ml-52 top-52 dark:text-white ">Mike Gester
+    </div>
+    <ul
+      class="flex flex-wrap text-sm font-medium text-center text-gray-400 bg-white rounded-lg ml-52 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800"
+      id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
+      <img class="absolute w-40 border-4 rounded-full border-emerald-500 dark:border-gray-800 top-40 left-80"
+        src="../assets/imgs/mikegester-logo.png" />
+      <li class="">
+        <button id="about-tab" data-tabs-target="#about" type="button" role="tab" aria-controls="about"
+          aria-selected="true"
+          class="inline-block p-4 border-2 border-t-0 border-l-0 border-r-0 text-emerald-500 dark:bg-emerald-700 border-b-emerald-500 hover:bg-emerald-50 dark:text-white bg-emerald-100 dark:hover:text-white">Timeline</button>
+      </li>
+      <li class="">
+        <button id="services-tab" data-tabs-target="#services" type="button" role="tab" aria-controls="services"
+          aria-selected="false"
+          class="inline-block p-4 text-gray-400 border-t-0 hover:border-1 hover:border-gray-100 border-x-0 hover:text-emerald-500 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:border-gray-800">Projects</button>
+      </li>
+      <li class="">
+        <button id="statistics-tab" data-tabs-target="#statistics" type="button" role="tab" aria-controls="statistics"
+          aria-selected="false"
+          class="inline-block p-4 border-t-0 border-x-0 hover:text-emerald-500 hover:bg-gray-100 dark:hover:text-gray-300 dark:hover:bg-gray-700 dark:border-gray-800">Services</button>
+      </li>
+      <li class="">
+        <button id="statistics-tab" data-tabs-target="#statistics" type="button" role="tab" aria-controls="statistics"
+          aria-selected="false"
+          class="inline-block p-4 border-t-0 border-x-0 hover:text-emerald-500 hover:bg-gray-100 dark:hover:text-gray-300 dark:hover:bg-gray-700 dark:border-gray-800">About</button>
+      </li>
+      <li class="">
+        <button id="dropdownButton" data-dropdown-toggle="dropdown"
+          class="inline-block p-4 bg-white border-t-0 border-x-0 hover:text-emerald-500 hover:bg-gray-100 dark:hover:text-white dark:hover:bg-gray-700 dark:bg-transparent dark:border-gray-800"
+          type=" button">
+          <span class="">More</span>
+
+        </button>
+      </li>
+
+    </ul><!-- Dropdown menu -->
+    <div id="dropdown"
+      class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700">
+      <ul class="py-1" aria-labelledby="dropdownButton">
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Gallery</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Videos</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Release
+            Notes</a>
+        </li>
+        <li>
+          <a href="#"
+            class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Testimonials</a>
         </li>
       </ul>
     </div>
   </div>
-</nav>
+
+  <!-- Dropdown menu -->
+  <div id="dropdown"
+    class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700">
+    <ul class="py-1" aria-labelledby="dropdownButton">
+      <li>
+        <a href="#"
+          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+      </li>
+      <li>
+        <a href="#"
+          class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Export
+          Data</a>
+      </li>
+      <li>
+        <a href="#"
+          class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+      </li>
+    </ul>
+  </div>
+
 
 </template>
