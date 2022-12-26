f84db9f327588eeef882c73909602039fa54cc3f
Mike Gester <mikegester.sabuga023@gmail.com>
2022-12-27 00:44:18 +0800

    2.0 mainjs  config update

--- src/main.js
+++ src/main.js
@@ -1,48 +1,36 @@
-import { createApp } from 'vue'
-import router from "./router"
-import App from './App.vue'
-import "./style.css"
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { Icon } from "@iconify/vue";
+import PerfectScrollbar from "vue3-perfect-scrollbar";
+import "vue3-perfect-scrollbar/dist/vue3-perfect-scrollbar.css";
+// import "flowbite";
+import "./assets/tailwind.css";
+import "./assets/animate.css";
+import "./assets/sass/css/hover.css";
+import "./assets/sass/css/scrollbar.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router, Icon);
+app.use(PerfectScrollbar);
+app.mount("#app");
 
-
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-// Change the icons inside the button based on previous settings
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    themeToggleLightIcon.classList.remove('hidden');
-} else {
-    themeToggleDarkIcon.classList.remove('hidden');
-}
-
-var themeToggleBtn = document.getElementById('theme-toggle');
-
-themeToggleBtn.addEventListener('click', function () {
-
-    // toggle icons inside button
-    themeToggleDarkIcon.classList.toggle('hidden');
-    themeToggleLightIcon.classList.toggle('hidden');
-
-    // if set via local storage previously
-    if (localStorage.getItem('color-theme')) {
-        if (localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        }
-
-        // if NOT set via local storage previously
-    } else {
-        if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        }
-    }
-
+router.beforeEach((to, from, next) => {
+    document.querySelector(".flex-sidebar").classList.add("hidden");
+    next();
 });
+
+const { verify } = require('hcaptcha');
+
+const secret = 'my hcaptcha secret from hcaptcha.com';
+const token = 'token from widget';
+
+verify(secret, token)
+    .then((data) => {
+        if (data.success === true) {
+            console.log('success!', data);
+        } else {
+            console.log('verification failed');
+        }
+    })
+    .catch(console.error);
