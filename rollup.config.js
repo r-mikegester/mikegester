773417b5bdf0e62d2e3df046aa6cf0854653bac2
Mike Gester <mikegester.sabuga023@gmail.com>
2022-12-27 03:40:07 +0800

    Added Roleup configs

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+// rollup.config.js
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+    input: 'main.js',
+    output: {
+        file: 'bundle.js',
+        format: 'iife',
+        name: 'MyModule'
+    },
+    plugins: [nodeResolve(), commonjs()]
+};
