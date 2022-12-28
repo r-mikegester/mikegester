8e1cac272d91797dc936310399539a08b3eb9fa1
Mike Gester <mikegester.sabuga023@gmail.com>
2022-12-29 01:35:00 +0800

    remove replit integration and custom css by acme

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -158,21 +158,6 @@
       </p>
 
       <div class="item mt-1">
-        <a href="" onclick="LoginWithReplit()" class="w-full flex text-left rounded-md box-border p-3 hover:bg-gray-100 text-gray-500
-          dark:hover:bg-gray-700">
-          <span class="mr-3 text-xl">
-            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd"
-                d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
-                clip-rule="evenodd"></path>
-            </svg>
-          </span>
-          <span class="w-full"> Login</span>
-        </a>
-
-      </div>
-
-      <div class="item mt-1">
         <router-link to="/comingsoon"
           class="w-full flex text-left rounded-md box-border p-3 hover:bg-gray-100 text-gray-500 dark:hover:bg-gray-700">
           <span class="mr-3 text-xl">
