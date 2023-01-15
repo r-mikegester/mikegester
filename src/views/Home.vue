b70f7656bf08ace069e5ab541df45d708ef0bc0f
Mike Gester <mikegester.sabuga023@gmail.com>
2023-01-15 20:29:55 +0800

    Added the blog featured on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,7 +29,7 @@
 
   </div>
 
-  <div class="container px-3 mt-10 mx-auto">
+  <div class="container px-3 mt-5 mx-auto">
 
     <!-- Section: Design Block -->
     <section class="mb-10 ">
@@ -58,7 +58,7 @@
     <div class="container  px-3 mx-auto">
 
       <!-- Section: Design Block -->
-      <section class="mb-10 -mt-16 md:-mt-0 text-gray-800 bg-emerald-400/40 rounded-3xl shadow-md">
+      <!--<section class="mb-10 -mt-16 md:-mt-0 text-gray-800 bg-emerald-400/40 rounded-3xl shadow-md">
 
 
         <div class="px-6 py-12 md:px-12 text-center lg:text-left">
@@ -104,7 +104,118 @@
             </div>
           </div>
         </div>
-      </section>
+      </section>-->
+
+      <div class="relative overflow-hidden rounded-3xl mb-10 bg-white dark:bg-gray-800 shadow-md">
+        <section class="overflow-hidden py-10 px-6 md:px-20 mx-auto lg:py-10">
+          <div class="container mx-auto">
+            <!-- Card Blog -->
+            <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+              <!-- Title -->
+              <div class="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+                <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Latest Blog</h2>
+                <p class="mt-1 text-gray-600 dark:text-gray-400">See how lifes going with people behind code, bars, and
+                  a cup of coffee.</p>
+              </div>
+              <!-- End Title -->
+
+              <!-- Grid -->
+              <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                <!-- Card -->
+                <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4]"
+                  href="#">
+                  <div class="aspect-w-16 aspect-h-11">
+                    <img class="w-full object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                      alt="Image Description">
+                  </div>
+                  <div class="my-6">
+                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
+                      Announcing a free plan for small teams
+                    </h3>
+                    <p class="mt-5 text-gray-600 dark:text-gray-400">
+                      At Wake, our mission has always been focused on bringing openness.
+                    </p>
+                  </div>
+                  <div class="mt-auto flex items-center gap-x-3">
+                    <img class="w-8 h-8 rounded-full" src="../assets/imgs/profile-pic.jpg" alt="Image Description">
+                    <div>
+                      <h5 class="text-sm text-gray-800 dark:text-gray-200">Mike Gester</h5>
+                    </div>
+                  </div>
+                </a>
+                <!-- End Card -->
+
+                <!-- Card -->
+                <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4]"
+                  href="#">
+                  <div class="aspect-w-16 aspect-h-11">
+                    <img class="w-full object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1562851529-c370841f6536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80"
+                      alt="Image Description">
+                  </div>
+                  <div class="my-6">
+                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
+                      How Google Assistant now helps you record stories for kids
+                    </h3>
+                    <p class="mt-5 text-gray-600 dark:text-gray-400">
+                      Google is constantly updating its consumer AI, Google Assistant, with new features.
+                    </p>
+                  </div>
+                  <div class="mt-auto flex items-center gap-x-3">
+                    <img class="w-8 h-8 rounded-full" src="../assets/imgs/profile-pic.jpg" alt="Image Description">
+                    <div>
+                      <h5 class="text-sm text-gray-800 dark:text-gray-200">Mike Gester</h5>
+                    </div>
+                  </div>
+                </a>
+                <!-- End Card -->
+
+                <!-- Card -->
+                <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4]"
+                  href="#">
+                  <div class="aspect-w-16 aspect-h-11">
+                    <img class="w-full object-cover rounded-xl"
+                      src="https://images.unsplash.com/photo-1521321205814-9d673c65c167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3548&q=80"
+                      alt="Image Description">
+                  </div>
+                  <div class="my-6">
+                    <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
+                      Front accounts - let's work together
+                    </h3>
+                    <p class="mt-5 text-gray-600 dark:text-gray-400">
+                      Are you an accountant? Are you a company formation advisor?
+                    </p>
+                  </div>
+                  <div class="mt-auto flex items-center gap-x-3">
+                    <img class="w-8 h-8 rounded-full" src="../assets/imgs/profile-pic.jpg" alt="Image Description">
+                    <div>
+                      <h5 class="text-sm text-gray-800 dark:text-gray-200">Mike Gester</h5>
+                    </div>
+                  </div>
+                </a>
+                <!-- End Card -->
+              </div>
+              <!-- End Grid -->
+
+              <!-- Card -->
+              <div class="mt-12 text-center">
+                <a class="inline-flex justify-center items-center gap-x-2 text-center bg-white border hover:border-gray-300 text-sm text-blue-600 hover:text-blue-700 font-medium hover:shadow-sm rounded-full focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:bg-slate-900 dark:border-gray-700 dark:hover:border-gray-600 dark:text-blue-500 dark:hover:text-blue-400 dark:hover:shadow-slate-700/[.7] dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800"
+                  href="#">
+                  Read more
+                  <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                    <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                      stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+                  </svg>
+                </a>
+              </div>
+              <!-- End Card -->
+            </div>
+            <!-- End Card Blog -->
+          </div>
+        </section>
+      </div>
+
       <!-- Section: Design Block -->
       <div class="relative overflow-hidden rounded-3xl bg-white dark:bg-gray-800 shadow-md">
         <section class="overflow-hidden py-10 px-6 md:px-20 mx-auto lg:py-10">
