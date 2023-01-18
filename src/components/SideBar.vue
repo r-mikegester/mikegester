25dbc3b505d61994b0a07e5e5012921de2df8eef
Mike Gester <mikegester.sabuga023@gmail.com>
2023-01-18 16:21:00 +0800

    single blog page now with categories to focus on

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -81,14 +81,49 @@
             <span class="w-full"> Contact</span>
           </router-link>
         </div>
-        <div class="item my-3">
-          <router-link to="/blog"
-            class="w-full flex text-left rounded-xl box-border p-3 text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700">
-            <span class="mr-3 text-xl">
+
+        <div class="my-3">
+
+          <menu-accordion>
+            <template v-slot:icon>
               <Icon icon="jam:newsletter-f" class="w-7 h-7" />
-            </span>
-            <span class="w-full"> Blog</span>
-          </router-link>
+            </template>
+            <template v-slot:title> Blogs </template>
+            <template v-slot:content>
+              <div class="ml-5">
+
+                <div class="item my-3">
+                  <router-link to="/themepicker"
+                    class="w-full flex text-left rounded-xl box-border p-3 text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <span class="mr-3 text-xl">
+                      <Icon icon="mdi:bike-fast" class="w-7 h-7" />
+                    </span>
+                    <span class="w-full"> Cycling</span>
+                  </router-link>
+                </div>
+                <div class="item my-3">
+                  <router-link to="/settings"
+                    class="w-full flex text-left rounded-xl box-border p-3 text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <span class="mr-3 text-xl">
+                      <Icon icon="line-md:document-code" class="w-7 h-7" />
+                    </span>
+                    <span class="w-full"> Programming</span>
+                  </router-link>
+                </div>
+
+                <div class="item mt-1">
+                  <router-link to="/toast"
+                    class="w-full flex text-left rounded-xl box-border p-3 text-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <span class="mr-3 text-xl">
+                      <Icon icon="mdi:aquarium" class="w-7 h-7" />
+                    </span>
+                    <span class="w-full"> Aquascaping</span>
+                  </router-link>
+                </div>
+              </div>
+            </template>
+          </menu-accordion>
+
         </div>
 
         <div class="item my-1 rounded-xl">
@@ -248,7 +283,26 @@
 
 
       </div>
-
+      <div
+        class="flex flex-col mt-10 bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800 dark:text-white">
+            Card title
+          </h3>
+          <p class="mt-2 text-gray-800 dark:text-gray-400">
+            With supporting text below as a natural lead-in to additional content.
+          </p>
+          <a class="mt-3 inline-flex items-center gap-2 mt-5 text-sm font-medium text-blue-500 hover:text-blue-700"
+            href="#">
+            Card link
+            <svg class="w-2.5 h-auto" width="16" height="16" viewBox="0 0 16 16" fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 1L10.6869 7.16086C10.8637 7.35239 10.8637 7.64761 10.6869 7.83914L5 14" stroke="currentColor"
+                stroke-width="2" stroke-linecap="round" />
+            </svg>
+          </a>
+        </div>
+      </div>
     </div>
 
   </nav>
